0a4e15a357c452555b5d7116de4eccec77193fe3
Alok Singh <aloksingh4748@gmail.com>
2026-09-23 14:21:36 +0530

    feat: add authenticated Socket.IO battle rooms

--- server/src/sockets/socketHandler.js
+++ server/src/sockets/socketHandler.js
@@ -1,4 +1,5 @@
 const { Server } = require("socket.io");
+const Battle = require("../models/Battle");
 const socketAuth = require("./socketAuth");
 
 const initializeSocket = (server) => {
@@ -14,6 +15,79 @@ const initializeSocket = (server) => {
   io.on("connection", (socket) => {
     console.log(
       `Authenticated socket: ${socket.user.username} (${socket.id})`
+    );
+
+    socket.on(
+      "join_battle_room",
+      async (receivedRoomCode, callback) => {
+        const sendResponse = (data) => {
+          if (typeof callback === "function") {
+            callback(data);
+          }
+        };
+
+        try {
+          const roomCode = receivedRoomCode
+            ?.trim()
+            .toUpperCase();
+
+          if (!roomCode) {
+            return sendResponse({
+              success: false,
+              message: "Room code is required",
+            });
+          }
+
+          const battle = await Battle.findOne({ roomCode });
+
+          if (!battle) {
+            return sendResponse({
+              success: false,
+              message: "Battle room not found",
+            });
+          }
+
+          const isBattlePlayer = battle.players.some(
+            (player) =>
+              player.user.toString() ===
+              socket.user._id.toString()
+          );
+
+          if (!isBattlePlayer) {
+            return sendResponse({
+              success: false,
+              message: "You are not a player in this battle",
+            });
+          }
+
+          socket.join(roomCode);
+          socket.data.roomCode = roomCode;
+
+          io.to(roomCode).emit("battle_room_updated", {
+            roomCode: battle.roomCode,
+            players: battle.players,
+            status: battle.status,
+          });
+
+          console.log(
+            `${socket.user.username} joined socket room ${roomCode}`
+          );
+
+          sendResponse({
+            success: true,
+            message: "Socket joined battle room",
+          });
+        } catch (error) {
+          console.error(
+            `Join socket room error: ${error.message}`
+          );
+
+          sendResponse({
+            success: false,
+            message: "Unable to join socket room",
+          });
+        }
+      }
     );
 
     socket.on("disconnect", (reason) => {
