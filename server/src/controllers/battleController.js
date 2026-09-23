8f3f0691e3dc608ac4a39046de995ebbefd6d4ee
Alok Singh <aloksingh4748@gmail.com>
2026-09-23 12:47:35 +0530

    feat: add battle room joining API

--- server/src/controllers/battleController.js
+++ server/src/controllers/battleController.js
@@ -37,6 +37,68 @@ const createBattle = async (req, res) => {
   }
 };
 
+const joinBattle = async (req, res) => {
+  try {
+    const roomCode = req.params.roomCode.toUpperCase();
+
+    const battle = await Battle.findOne({ roomCode });
+
+    if (!battle) {
+      return res.status(404).json({
+        success: false,
+        message: "Battle room not found",
+      });
+    }
+
+    if (battle.status !== "waiting") {
+      return res.status(400).json({
+        success: false,
+        message: "Battle has already started",
+      });
+    }
+
+    const alreadyJoined = battle.players.some(
+      (player) =>
+        player.user.toString() === req.user._id.toString()
+    );
+
+    if (alreadyJoined) {
+      return res.status(400).json({
+        success: false,
+        message: "You have already joined this room",
+      });
+    }
+
+    if (battle.players.length >= 2) {
+      return res.status(400).json({
+        success: false,
+        message: "Battle room is full",
+      });
+    }
+
+    battle.players.push({
+      user: req.user._id,
+      username: req.user.username,
+    });
+
+    await battle.save();
+
+    res.status(200).json({
+      success: true,
+      message: "Battle room joined successfully",
+      battle,
+    });
+  } catch (error) {
+    console.error(`Join battle error: ${error.message}`);
+
+    res.status(500).json({
+      success: false,
+      message: "Unable to join battle room",
+    });
+  }
+};
+
 module.exports = {
   createBattle,
+  joinBattle,
 };
--- server/src/routes/battleRoutes.js
+++ server/src/routes/battleRoutes.js
@@ -1,9 +1,13 @@
 const express = require("express");
-const { createBattle } = require("../controllers/battleController");
+const {
+  createBattle,
+  joinBattle,
+} = require("../controllers/battleController");
 const protect = require("../middleware/authMiddleware");
 
 const router = express.Router();
 
 router.post("/", protect, createBattle);
+router.post("/:roomCode/join", protect, joinBattle);
 
 module.exports = router;
